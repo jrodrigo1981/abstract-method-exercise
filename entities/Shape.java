a5edf0fe7f87e5ee23431c512aa26e958a010841
jrodrigo1981 <jrodrigo1981@gmail.com>
2021-07-16 13:21:10 -0300

    Feito a implementação do Enum.

---
+++ entities/Shape.java
@@ -0,0 +1,7 @@
+package com.rodrigo.aula133.entities;
+
+public class Shape {
+	
+	
+
+}
---
+++ entities/enums/Color.java
@@ -0,0 +1,8 @@
+package com.rodrigo.aula133.entities.enums;
+
+public enum Color {
+	BLACK,
+	BLUE,
+	RED;
+
+}
